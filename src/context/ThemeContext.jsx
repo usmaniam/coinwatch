7a0ffe04cc3cd6b3342e313b025565f76f2e70b3
Usman Abubakar <usmanuiam@gmail.com>
2023-02-06 18:18:07 +0100

    App theme and light/dark mode implemented

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,46 @@
+import {useState, useEffect, createContext} from 'react'
+
+const getInitialTheme = () => {
+    if (typeof window !== 'undefined' && window.localStorage) {
+        const storedPrefs = window.localStorage.getItem('color-theme')
+        if (typeof storedPrefs === 'string') {
+            return storedPrefs
+        }
+        
+        const userMedia = window.matchMedia('()prefers-color-scheme: dark')
+        if (userMedia.matches) {
+            return 'dark'
+        }
+    }
+    return 'light'
+}
+
+export const ThemeContext = createContext()
+
+export const ThemeProvider = ({initialTheme, children}) => {
+    const [theme, setTheme] = useState(getInitialTheme)
+
+    const rawSetTheme = (theme) => {
+        const root = window.document.documentElemment;
+        const isDark = theme === 'dark'
+
+        root.classlist.remove(isDark ? 'light' : 'dark')
+        root.classlist.add(theme)
+
+        localStorage.setIteam('color-theme', theme)
+    }
+
+    if (initialTheme) {
+        rawSetTheme(initialTheme)
+    }
+
+    useEffect(()=>{
+        rawSetTheme(theme)
+    },[theme])
+
+    return (
+        <ThemeContext.Provider value={{theme, setTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
